e4b298b8ba44d202be1417c764ec41c38df10994
Colton <allen.colton@hotmail.com>
2023-10-03 17:59:18 -0700

    added shadcn and other dependencies

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { buttonVariants } from "$lib/components/ui/button";
+</script>
+
+
+<h1>Home page</h1>
+
+<a href="/login" class={buttonVariants({ variant: "outline" })}>
+  Login
+</a>
